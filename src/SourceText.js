b80d638ac6bb9bde85bffc7a5da77f0ab8031b5f
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2013-07-30 21:09:42 +0900

    change file layout.

---
+++ src/SourceText.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string} str
+ * @constructor
+ */
+function SourceText(str) {
+  this.text = str;
+}
+
+SourceText.prototype = {
+  /**
+   * @param {string} str
+   */
+  moveAfter: function(str) {
+    var index = this.text.indexOf(str);
+    this.text = index < 0 ? '' : this.text.substring(index + str.length);
+  },
+
+  /**
+   * @param {string} start
+   * @param {string} end
+   * @return {string}
+   */
+  getBetween: function(start, end) {
+
+    var s = this.text.indexOf(start);
+    if (s < 0) {
+      return null;
+    }
+    s += start.length;
+
+    var e = this.text.indexOf(end);
+    if (e < 0) {
+      return null;
+    }
+
+    return this.text.substring(s, e);
+  },
+
+  /**
+   * @param {string} str
+   * @return {number}
+   */
+  indexOf: function(str) {
+    return this.text.indexOf(str);
+  }
+};
